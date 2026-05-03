16d72a01dcb11a64940a9190bb6338f5cf9df3eb
雨薇. <vumanhsan87@gmail.com>
2026-05-03 12:46:48 +0800

    Add trailingSlash for static export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/myuniverse',
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
