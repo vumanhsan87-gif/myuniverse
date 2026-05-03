9406a1413c71978f51419c74f6873c0f96a84dbd
雨薇. <vumanhsan87@gmail.com>
2026-05-03 13:21:08 +0800

    Remove duplicate basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/myuniverse',
   trailingSlash: true,
   images: {
     unoptimized: true,
